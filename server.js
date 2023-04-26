015e818e6cc6fbd94fd83a72b46847a82d674da6
yashkr90 <kryash90@gmail.com>
2023-04-27 00:57:45 +0530

    mongo

--- server.js
+++ server.js
@@ -21,31 +21,25 @@ app.use("/", Routes);
 
 const data = JSON.parse(fs.readFileSync("./sample_data.json", "utf-8"));
 
-  // console.log(data);
+// console.log(data);
 
-  const importData = async () => {
-    try {
-      await Data.insertMany(data,{ordered : false });
-      console.log("data successfully imported");
-      // to exit the process
-      process.exit();
-    } catch (error) {
-      console.log("error");
-    }
-  };
+const importData = async () => {
+  try {
+    await Data.insertMany(data, { ordered: false });
+    console.log("data successfully imported");
+    // to exit the process
+    process.exit();
+  } catch (error) {
+    console.log("error");
+  }
+};
 
-  
 Connection(process.env.MONGOURL).then((res) => {
   console.log(res);
   if (res === "success") {
-    
-    
-
     app.listen(port || process.env.PORT, () =>
       console.log(`Server is running on PORT ${port}`)
     );
-    
-    
   }
 });
 importData();
